d154a22d5e981511d3b972986d945a3c774f70f8
Stephanie Lakner <stephanie.lakner@rub.de>
2022-11-13 16:35:41 +0100

    some work on parsing

---
+++ philo/parse.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 16:02:22 by slakner           #+#    #+#             */
+/*   Updated: 2022/11/13 16:34:26 by slakner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+t_simulation	read_args(int argc, char **argv)
+{
+	int				i;
+	t_simulation	sim;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (!validate_arg(argv[i]))
+			return (NULL);
+		i++;
+	}
+	sim.number_philosophers = ft_atoi(argv[1]);
+	sim.time_die = ft_atoi(argv[2]);
+	sim.time_eat = ft_atoi(argv[3]);
+	sim.time_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		sim.must_eat_times = ft_atoi(argv[5]);
+	return (sim);
+}
+
+int	validate_arg(char *arg)
+{
+	while (arg)
+	{
+		if (*arg < '0' || *arg > '9')
+			return (0);
+		arg++;
+	}
+	return (1);
+}
+
+int	is_whitespace(const char *c)
+{
+	if (*c == ' ' || *c == '\n' || *c == '\t'
+		|| *c == '\v' || *c == '\f' || *c == '\r')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	number;
+	int	digit;
+	int	i;
+	int	positive_negative;
+
+	number = 0;
+	positive_negative = 1;
+	i = 0;
+	while (is_whitespace(str))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			positive_negative *= -1;
+		str ++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		digit = str[i] - '0';
+		number = number * 10 + digit;
+		i++;
+	}
+	return (number * positive_negative);
+}
--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,11 +6,29 @@
 /*   By: slakner <slakner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 15:57:57 by slakner           #+#    #+#             */
-/*   Updated: 2022/11/13 15:58:15 by slakner          ###   ########.fr       */
+/*   Updated: 2022/11/13 16:31:18 by slakner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include "philosophers.h"
+
+int	main(int argc, char **argv)
 {
-	
+	t_simulation	sim;
+
+	if (argc < 5 || argc > 6)
+	{
+		printf("Wrong number of arguments.\n")
+		print_usage();
+		return (1);
+	}
+	sim = read_args(argc, argv);
+	return (eat_sleep_die(sim));
+}
+
+void	print_usage(void)
+{
+	printf("Usage:\n");
+	printf("philo <number_of_philosophers> <time_to_die> <time_to_eat> ");
+	printf("<time_to_sleep> [number_of_times_each_philosopher_must_eat]");
 }
